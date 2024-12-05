69ab78fd195e56e1fe960dd2b6cebd76e9dfb993
ArunReddy99-ux <arunreddyveerelly2003@gmail.com>
2024-12-05 16:23:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TipCalculator />
     </div>
   );
 }
-
-export default App;
+//children prop usage work
+function TipCalculator() {
+  const [amount, setamount] = useState("");
+  const [amt, setamt] = useState(0);
+  const [frndamt, setfrndamt] = useState(0);
+  const [isOpen, setisOpen] = useState(false);
+  function handleClick() {
+    setamount("");
+    setamt(0);
+    setfrndamt(0);
+    setisOpen(false);
+  }
+  return (
+    <div>
+      <Bill amount={amount} setamount={setamount} setOpen={setisOpen} />
+      <Selectpercent amt={amt} onSelect={setamt}>
+        How would like the servive?
+      </Selectpercent>
+      <Selectpercent amt={frndamt} onSelect={setfrndamt}>
+        How would friend like the servive?
+      </Selectpercent>
+      <Ttl at={amount} fa={frndamt} mat={amt} onOpen={isOpen} />
+      <Reset onClick={handleClick} onOpen={isOpen} />
+    </div>
+  );
+}
+function Bill({ amount, setamount, setOpen }) {
+  function handleChange(e) {
+    setamount(e);
+    setOpen(true);
+  }
+  return (
+    <div>
+      <label>How much was the bill?</label>
+      <input
+        type="text"
+        value={amount}
+        placeholder="Bill value"
+        onChange={(e) => handleChange(Number(e.target.value))}
+      />
+    </div>
+  );
+}
+function Selectpercent({ amt, onSelect, children }) {
+  function handleselection1(e) {
+    onSelect(e);
+  }
+  return (
+    <div>
+      <div>
+        <label>{children}</label>
+        <select
+          value={amt}
+          onChange={(e) => handleselection1(Number(e.target.value))}
+        >
+          <option value="0">Dissatisfied (0%)</option>
+          <option value="5">It was Okay (5%)</option>
+          <option value="10">It was good(10%)</option>
+          <option value="20">Absolutely Amazing(20%)</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+function Ttl({ at, fa, mat, onOpen }) {
+  let tip = (at * ((fa + mat) / 2)) / 100;
+  return (
+    <div>
+      {onOpen ? (
+        <b>
+          <p>
+            You Should Pay {at + (at * ((fa + mat) / 2)) / 100}(
+            {`${at}+ $${tip} tip`})
+          </p>
+        </b>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+function Reset({ onClick, onOpen }) {
+  return <div>{onOpen ? <button onClick={onClick}>RESET</button> : ""}</div>;
+}
